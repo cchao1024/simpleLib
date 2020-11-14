5c8aa0ff3117f9c3d42fc236754cfe477379e916
cchao1024 <cchao1024@163.com>
2020-11-14 15:19:39 +0800

    CollectionHelper

--- src/main/java/com/cchao/simplelib/core/CollectionHelper.java
+++ src/main/java/com/cchao/simplelib/core/CollectionHelper.java
@@ -39,4 +39,12 @@ public class CollectionHelper {
         }
         return false;
     }
+
+    public static <T> T getFirst(List<T> list) {
+        return list != null && !list.isEmpty() ? list.get(0) : null;
+    }
+
+    public static <T> T getLast(List<T> list) {
+        return list != null && !list.isEmpty() ? list.get(list.size() - 1) : null;
+    }
 }
